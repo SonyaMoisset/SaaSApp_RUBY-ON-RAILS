15ee08799ff57d6bd04cd1ade89cbebee9d0c70c
Sonya Moisset <sonya.moisset@gmail.com>
2017-08-13 22:38:12 +0000

    Clean up contact form routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
   get 'about', to: 'pages#about'
   
-  resources :contacts
+  resources :contacts, only: :create
   
-  get 'contact-us', to: 'contacts#new'
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
