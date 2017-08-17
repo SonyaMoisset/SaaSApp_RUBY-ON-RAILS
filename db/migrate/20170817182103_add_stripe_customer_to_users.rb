c7f9b90e58b844e72d59ee4b1f43c018e1743b7f
Sonya Moisset <sonya.moisset@gmail.com>
2017-08-17 18:22:36 +0000

    Add stripe token to users table

---
+++ db/migrate/20170817182103_add_stripe_customer_to_users.rb
@@ -0,0 +1,5 @@
+class AddStripeCustomerToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :stripe_customer_token, "string"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816210803) do
+ActiveRecord::Schema.define(version: 20170817182103) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170816210803) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "plan_id"
+    t.string   "stripe_customer_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
