95d2c183f82752252c670a56995aab5f891fc561
Sonya Moisset <sonya.moisset@gmail.com>
2017-08-20 10:45:03 +0000

    Add profile and user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
   belongs_to :plan
+  has_one :profile
   
   attr_accessor :stripe_card_token
   def save_with_subscription
